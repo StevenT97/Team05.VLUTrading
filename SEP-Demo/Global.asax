2912533bdc75ca4231f1b55e091f196e3f53c0a2
T155100 - Vo Quang Thien - K21T03 <thienvo8@vanlanguni.vn>
2018-04-21 23:01:49 +0700

    Add Project

---
+++ SEP-Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SEP_Demo.MvcApplication" Language="C#" %>
---
+++ SEP-Demo/Views/Account/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEP_Demo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult ForgetPassword()
+        {
+            return View();
+        }
+    }
+}
